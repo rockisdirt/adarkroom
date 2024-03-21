7e11a342262d20404babdccf34abd36af59a16e0
rockisdirt <124087276+rockisdirt@users.noreply.github.com>
2024-03-21 14:12:59 -0700

    Update room.js

--- script/room.js
+++ script/room.js
@@ -6,7 +6,7 @@ var Room = {
 	_FIRE_COOL_DELAY: 5 * 60 * 1000, // time after a stoke before the fire cools
 	_ROOM_WARM_DELAY: 30 * 1000, // time between room temperature updates
 	_BUILDER_STATE_DELAY: 0.5 * 60 * 1000, // time between builder state updates
-	_STOKE_COOLDOWN: 10, // cooldown to stoke the fire
+	_STOKE_COOLDOWN: 0, // cooldown to stoke the fire
 	_NEED_WOOD_DELAY: 15 * 1000, // from when the stranger shows up, to when you need wood
 	buttons: {},
 	Craftables: {
